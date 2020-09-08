aec94db53f2c360e16dd8b181f45b75a31b5a0c9
Sakhawat hosen <sakhawatcmt@gmail.com>
2020-09-08 16:15:35 +0600

    ABP.IO CRUD Completed

---
+++ src/Acme.BookStore.Application.Contracts/IBookAppService.cs
@@ -0,0 +1,13 @@
+﻿using Acme.BookStore.Books;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Acme.BookStore
+{
+    public interface IBookAppService :ICrudAppService<BookDto, Guid, PagedAndSortedResultRequestDto, CreateUpdateBookDto>
+    {
+    }
+}
